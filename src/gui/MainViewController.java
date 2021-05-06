75923f444b9c5c24cacfba36d6fcd672358eea9a
norb_gf <norb_gf@yahoo.com.br>
2021-05-06 07:54:41 -0300

    Parametro inicializacao lambda

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -48,8 +49,11 @@ public class MainViewController implements Initializable {
 	}
 
 	public void onMenuItemAtividadesListAction() {
-//		loadView("/gui/AtividadesList.fxml");
-		loadView2("/gui/AtividadesList.fxml");
+		loadView("/gui/AtividadesList.fxml",
+				(AtividadesListController controller) -> {
+					controller.setAtividadesService(new AtividadesService());
+					controller.updateTableView();
+				});
 	}
 	
 	public void onMenuItemContasBancariasAction() {
@@ -73,7 +77,7 @@ public class MainViewController implements Initializable {
 	}
 
 	public void onMenuItemAboutAction() {
-		loadView("/gui/Sobre.fxml");
+		loadView("/gui/Sobre.fxml", x -> {});
 	}
 
 	@Override
@@ -81,27 +85,9 @@ public class MainViewController implements Initializable {
 
 	}
 
-	private synchronized void loadView(String absoluteName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-
-		} catch (IOException e) {
-			Alerts.showAlert("IOException", "Erro no loading View", e.getMessage(), AlertType.ERROR);
-		}
-	}
-
-// temporario ============================**
-	
-	private synchronized void loadView2(String absoluteName) {
+	// <T> ver conceito de Generics
+	// <Consumer> ver conceito de expressões lambda  
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction)  {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
@@ -114,15 +100,11 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			AtividadesListController controller = loader.getController();
-			controller.setAtividadesService(new AtividadesService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 
 		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Erro no loading View", e.getMessage(), AlertType.ERROR);
 		}
 	}
-// temporario ****************************==
-	
-
 }
